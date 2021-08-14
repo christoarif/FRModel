d95b40d6f64ac74b2dd905848743cbbd2ec0e1a8
Evening <johnchangqi@hotmail.com>
2021-08-14 15:08:18 +0800

    Update fixture with new path

--- tests/base/D2Fixture/test_fixture.py
+++ tests/base/D2Fixture/test_fixture.py
@@ -2,17 +2,16 @@ import os
 import unittest
 
 from frmodel.base.D2 import Frame2D
+from tests.path import RSC_PATH
 
 _DIR = os.path.dirname(os.path.realpath(__file__))
 _RSC = _DIR + "/../../../rsc"
-
-
 class TestD2Fixture(unittest.TestCase):
 
     @classmethod
     def setUp(cls) -> None:
-        cls.frame_box = Frame2D.from_image(f"{_DIR}/box.png")
-        cls.frame = Frame2D.from_image(f"{_DIR}/sample.jpg")
+        cls.frame_box = Frame2D.from_image(RSC_PATH + f"imgs/rgb/basic/box.png")
+        cls.frame = Frame2D.from_image(RSC_PATH + f"imgs/rgb/chestnut/test_0.jpg")
         cls.window = 100
         cls._RSC = _RSC
         cls.channels = 3
