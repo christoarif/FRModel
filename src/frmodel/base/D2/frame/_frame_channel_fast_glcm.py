c07c7b0e06be5c666c5ce0d7558f38ab9a887400
Evening <johnchangqi@hotmail.com>
2021-08-23 15:04:52 +0800

    Change GLCM to adhere to new standards as a standalone call

--- src/frmodel/base/D2/frame/_frame_channel_fast_glcm.py
+++ src/frmodel/base/D2/frame/_frame_channel_fast_glcm.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass, field
-from typing import Tuple, List, TYPE_CHECKING, Union
+from typing import Tuple, List, TYPE_CHECKING, Union, Iterable
 
 import numpy as np
 
@@ -12,24 +12,9 @@ from frmodel.base.D2.frame._cy_fast_glcm2 import CyGLCM
 if TYPE_CHECKING:
     from frmodel.base.D2.frame2D import Frame2D
 
+
 class _Frame2DChannelFastGLCM(ABC):
     """ This re-implements Wang Jifei's Fast GLCM Script by adding the option of binarization. """
-
-    @dataclass
-    class GLCM:
-        """ This holds all GLCM parameters to pass into get_glcm
-
-        Note that contrast, correlation, asm takes arguments similarly to how get_chns work.
-
-        Entropy has been replaced by ASM. Angular Second Moment
-
-        e.g. contrast=[f.CHN.HSV]
-        """
-        by:     int = 1
-        radius: int = 2
-        bins:   int = 8
-
-        channels: List[CONSTS.CHN] = field(default_factory=lambda: [])
 
     def CON(self, chns: Union[List[str], str]):  return self[:, :, [f"CON_{i}" for i in chns] if isinstance(chns, List) else chns]
     def COR(self, chns: Union[List[str], str]):  return self[:, :, [f"COR_{i}" for i in chns] if isinstance(chns, List) else chns]
@@ -37,26 +22,35 @@ class _Frame2DChannelFastGLCM(ABC):
     def MEAN(self, chns: Union[List[str], str]): return self[:, :, [f"MEAN_{i}" for i in chns] if isinstance(chns, List) else chns]
     def VAR(self, chns: Union[List[str], str]):  return self[:, :, [f"VAR_{i}" for i in chns] if isinstance(chns, List) else chns]
 
-    def get_glcm(self: 'Frame2D', glcm:GLCM) -> Tuple[np.ndarray, List[str]]:
+    def get_glcm(self: 'Frame2D',
+                 chns: Iterable[Frame2D.CHN] = (),
+                 radius: int = 2,
+                 bins: int = 8):
         """ This will get the GLCM statistics for this window
 
         Details on how GLCM works is shown on the wiki.
 
-        :param glcm: A GLCM Class, this class holds all parameters.
+        :param chns: Channels, can be also in strings.
+        :param radius: Radius of GLCM Window
+        :param bins: Bin size pre-processing of GLCM.
         """
 
         # FAST GLCM
-        channels = glcm.channels if glcm.channels else self.labels.keys()
-        data = CyGLCM(self[..., channels].data.astype(np.float32), glcm.radius, glcm.bins)\
-            .create_glcm()
-        data = data.swapaxes(-2,-1).reshape([*data.shape[:2], -1])
+        chns = chns if chns else list(self.labels.keys())
+        data = CyGLCM(self[..., chns].data.astype(np.float32), radius, bins).create_glcm()
+        data = data.swapaxes(-2, -1).reshape([*data.shape[:2], -1])
 
         labels = []
 
-        labels.extend(CONSTS.CHN.GLCM.CON( list(self._util_flatten(channels))))
-        labels.extend(CONSTS.CHN.GLCM.COR( list(self._util_flatten(channels))))
-        labels.extend(CONSTS.CHN.GLCM.ASM( list(self._util_flatten(channels))))
-        labels.extend(CONSTS.CHN.GLCM.MEAN(list(self._util_flatten(channels))))
-        labels.extend(CONSTS.CHN.GLCM.VAR( list(self._util_flatten(channels))))
+        labels.extend(CONSTS.CHN.GLCM.CON( list(self._util_flatten(chns))))
+        labels.extend(CONSTS.CHN.GLCM.COR( list(self._util_flatten(chns))))
+        labels.extend(CONSTS.CHN.GLCM.ASM( list(self._util_flatten(chns))))
+        labels.extend(CONSTS.CHN.GLCM.MEAN(list(self._util_flatten(chns))))
+        labels.extend(CONSTS.CHN.GLCM.VAR( list(self._util_flatten(chns))))
 
-        return data, labels
+        self._data = self.crop_glcm(radius, glcm_by=1).data
+        t = self.append(data, labels=labels)
+        self._data = t.data
+        self._labels = t.labels
+
+        return self
